a850c937c4d632834ecf3e79948566dcaa32cd15
Mohamed Rifayath <141167029+mohamedrifa@users.noreply.github.com>
2025-05-30 22:02:39 +0530

    30/05/2025
    .

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -30,36 +30,36 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   void initState() {
     super.initState();
     // Trigger size animation after 3 seconds
-    Future.delayed(const Duration(seconds: 3), () {
-      setState(() {
-        leftOffset = -403;
+    // Future.delayed(const Duration(seconds: 3), () {
+    //   setState(() {
+    //     leftOffset = -403;
         
-        greenWidth = 337.6;
-        greenHeight = 41.82;
-        greenLeft = 56 - 187.36;
-        greenBottom = 36 - 151.14;
+    //     greenWidth = 337.6;
+    //     greenHeight = 41.82;
+    //     greenLeft = 56 - 187.36;
+    //     greenBottom = 36 - 151.14;
 
-        yellowWidth = 19.5;
-        yellowHeight = 593.67;
-        yellowRight = 27 - 324.99;
-        yellowBottom = 26 - 115.04;
+    //     yellowWidth = 19.5;
+    //     yellowHeight = 593.67;
+    //     yellowRight = 27 - 324.99;
+    //     yellowBottom = 26 - 115.04;
 
-        darkWidth = 1223.27;
-        darkHeight = 1060.32;
-        darkTop = 62 - 330.07;
-        darkRight = 15 - 402.61;
-      });
-    });
+    //     darkWidth = 1223.27;
+    //     darkHeight = 1360.32;
+    //     darkTop = 62 - 410.07;
+    //     darkRight = 15 - 482.61;
+    //   });
+    // });
     // Navigate to HomeScreen after animation
-    Future.delayed(const Duration(seconds: 4), () {
-      Navigator.of(context).pushReplacement(PageRouteBuilder(
-        transitionDuration: const Duration(milliseconds: 1500),
-        pageBuilder: (_, __, ___) => TaskScreen(),
-        transitionsBuilder: (_, animation, __, child) {
-          return FadeTransition(opacity: animation, child: child);
-        },
-      ));
-    });
+    // Future.delayed(const Duration(seconds: 4), () {
+    //   Navigator.of(context).pushReplacement(PageRouteBuilder(
+    //     transitionDuration: const Duration(milliseconds: 1500),
+    //     pageBuilder: (_, __, ___) => TaskScreen(),
+    //     transitionsBuilder: (_, animation, __, child) {
+    //       return FadeTransition(opacity: animation, child: child);
+    //     },
+    //   ));
+    // });
 
 
   }
@@ -67,6 +67,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: const Color(0xFF2C9C94),
       body: Stack(
@@ -75,32 +76,32 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           AnimatedPositioned(
             duration: const Duration(milliseconds: 600),
             curve: Curves.easeInOut,
-            top: MediaQuery.of(context).size.height / 2 - 100,
-            left: screenWidth / 2 - 200 + leftOffset,
+            top: screenHeight / 2 - 100,
+            left: screenWidth / 2 - ((screenWidth * 1.03)/2) + leftOffset,
             child: SizedBox(
-              width: 400,
+              width: screenWidth * 1.03,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   Image.asset(
                     'assets/logo.png',
-                    height: 73,
+                    width: 0.4045 * screenWidth,
                   ),
-                  const SizedBox(height: 36),
-                  const Text(
+                  SizedBox(height: 0.03 * screenHeight),
+                  Text(
                     "Stay On Track. Stay In Flow",
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       color: Colors.white,
                       fontFamily: 'Poppins-Regular',
-                      fontSize: 24,
+                      fontSize: 0.0582 * screenWidth,
                       fontWeight: FontWeight.w500,
                     ),
                   ),
-                  const SizedBox(height: 10.5),
+                  SizedBox(height: 0.0114 * screenHeight),
                   Image.asset(
                     'assets/dots.png',
-                    height: 32,
+                    width: 0.6407 * screenWidth,
                   ),
                 ],
               ),
@@ -110,15 +111,15 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           AnimatedPositioned(
             duration: const Duration(milliseconds: 600),
             curve: Curves.easeInOut,
-            bottom: greenBottom,
-            left: greenLeft,
+            bottom: greenBottom / 917 * screenHeight,
+            left: greenLeft / 412 * screenWidth,
             child: Transform.rotate(
               angle: 41.29 * 3.1416 / 180,
               child: AnimatedContainer(
                 duration: const Duration(milliseconds: 600),
                 curve: Curves.easeInOut,
-                width: greenWidth,
-                height: greenHeight,
+                width: greenWidth / 412 * screenWidth,
+                height: greenHeight / 917 * screenHeight,
                 decoration: const BoxDecoration(
                   color: Color(0xFFBFF6E5),
                   borderRadius: BorderRadius.all(Radius.circular(36)),
@@ -138,15 +139,15 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           AnimatedPositioned(
             duration: const Duration(milliseconds: 600),
             curve: Curves.easeInOut,
-            bottom: yellowBottom,
-            right: yellowRight,
+            bottom: yellowBottom / 917 * screenHeight,
+            right: yellowRight / 412 * screenWidth,
             child: Transform.rotate(
               angle: 41.29 * 3.1416 / 180,
               child: AnimatedContainer(
                 duration: const Duration(milliseconds: 600),
                 curve: Curves.easeInOut,
-                width: yellowWidth,
-                height: yellowHeight,
+                width: yellowWidth / 412 * screenWidth,
+                height: yellowHeight / 917 * screenHeight,
                 decoration: const BoxDecoration(
                   color: Color(0xFFFEDB8A),
                   borderRadius: BorderRadius.all(Radius.circular(34)),
@@ -166,15 +167,15 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           AnimatedPositioned(
             duration: const Duration(milliseconds: 600),
             curve: Curves.easeInOut,
-            top: darkTop,
-            right: darkRight,
+            top: darkTop / 917 * screenHeight,
+            right: darkRight / 412 * screenWidth,
             child: Transform.rotate(
               angle: 41.29 * 3.1416 / 180,
               child: AnimatedContainer(
                 duration: const Duration(milliseconds: 600),
                 curve: Curves.easeInOut,
-                width: darkWidth,
-                height: darkHeight,
+                width: darkWidth / 412 * screenWidth,
+                height: darkHeight / 917 * screenHeight,
                 decoration: const BoxDecoration(
                   color: Color(0xFF313036),
                   borderRadius: BorderRadius.all(Radius.circular(43)),
