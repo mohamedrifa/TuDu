165acb6712324e1af6329a3406ab61bcd9419a0e
Mohamed Rifayath <141167029+mohamedrifa@users.noreply.github.com>
2025-09-02 15:07:15 +0530

    migration completed

--- lib/database/hive_service.dart
+++ lib/database/hive_service.dart
@@ -1,7 +1,6 @@
 // services/hive_service.dart
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
-import '../models/task.dart';
 import '../models/settings.dart';
 
 class HiveService {
@@ -10,19 +9,10 @@ class HiveService {
       final appDocumentDirectory = 
           await path_provider.getApplicationDocumentsDirectory();
       Hive.init(appDocumentDirectory.path);
-  
-      if (!Hive.isAdapterRegistered(0)) Hive.registerAdapter(TaskAdapter());
       if (!Hive.isAdapterRegistered(1)) Hive.registerAdapter(SettingsAdapter());
-  
-      await Hive.openBox<Task>('tasks');
       await Hive.openBox<AppSettings>('settings');
     } catch (e) {
       print("Hive initialization error: $e");
     }
-  }
-
-
- static Box<Task> getTasksBox() {
-    return Hive.box<Task>('tasks'); 
   }
 }
